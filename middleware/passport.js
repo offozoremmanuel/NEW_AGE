e397a52414255958ff1df41e917fa3a8acf622f3
offozoremmanuel <offozoremmy@gmail.com>
2026-05-25 18:05:45 +0100

    hosting

--- middleware/passport.js
+++ middleware/passport.js
@@ -2,36 +2,42 @@ const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const customerModel = require('../models/customer')
 
-passport.use(new GoogleStrategy({
-    clientID:process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL
-  },
- async(accessToken, refreshToken, profile, cb) => {
-    try {
-        console.log(profile)
-        let user = await customerModel.findOne({email: profile._json.email})
+const googleOAuthIsConfigured = process.env.GOOGLE_CLIENT_ID &&
+  process.env.GOOGLE_CLIENT_SECRET &&
+  process.env.GOOGLE_CALLBACK_URL;
 
-        if(!user){
-            const [firstName, ...lastNameParts] = (profile._json.name || '').split(' ');
+if (googleOAuthIsConfigured) {
+  passport.use(new GoogleStrategy({
+      clientID:process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL
+    },
+  async(accessToken, refreshToken, profile, cb) => {
+      try {
+          console.log(profile)
+          let user = await customerModel.findOne({email: profile._json.email})
 
-            user = new customerModel({
-                firstName: profile._json.given_name,
-                lastName: profile._json.family_name,
-                email:profile._json.email,
-                googleId: profile.id,
-                isVerified:profile._json.email_verified,
-                profilePicture: profile._json.picture
-            })
-            await user.save()
-        }
-        return cb(null, user)
-    } catch (error) {
-        console.log('error signing up with google', error.message)
-        return cb(error)
+          if(!user){
+              const [firstName, ...lastNameParts] = (profile._json.name || '').split(' ');
+
+              user = new customerModel({
+                  firstName: profile._json.given_name,
+                  lastName: profile._json.family_name,
+                  email:profile._json.email,
+                  googleId: profile.id,
+                  isVerified:profile._json.email_verified,
+                  profilePicture: profile._json.picture
+              })
+              await user.save()
+          }
+          return cb(null, user)
+      } catch (error) {
+          console.log('error signing up with google', error.message)
+          return cb(error)
+      }
     }
-  }
-));
+  ));
+}
 
 passport.serializeUser((user, cb) => {
   cb(null, user.id);
@@ -50,9 +56,19 @@ passport.deserializeUser(async (id, cb) => {
     }
 });
 
-const profile= passport.authenticate('google', {scope:  ['profile', 'email']})
+const googleOAuthNotConfigured = (req, res) => {
+  res.status(503).json({
+    message: 'Google OAuth is not configured'
+  })
+}
 
-const loginProfile = passport.authenticate('google', {failureRedirect:'/login'})
+const profile = googleOAuthIsConfigured
+  ? passport.authenticate('google', {scope:  ['profile', 'email']})
+  : googleOAuthNotConfigured
+
+const loginProfile = googleOAuthIsConfigured
+  ? passport.authenticate('google', {failureRedirect:'/login'})
+  : googleOAuthNotConfigured
 
 module.exports ={
     passport,
