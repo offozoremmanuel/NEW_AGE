bd1b60abf66fa2b5c80df4a59ed697a25ab3ab5c
PrimeOsita <brightstarositadinma@gmail.com>
2026-05-26 07:59:20 +0100

    Fixed some Route issues linking to swagger

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const cors =require('cors');
 const PORT = process.env.PORT || 6677;
+require('./controller/orderController')
 const customerRoute = require('./routes/customer');
 const orderRouter = require('./routes/order')
 const axios = require('axios');
